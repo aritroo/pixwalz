6bb46520c4b23d4c60241d2c9678955cea959ca3
Aritroo Chowdhury <aritroo123chowdhury52@gmail.com>
2023-04-09 00:59:46 +0530

    orentation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pixwalz/views/screens/home.dart';
 import 'package:sizer/sizer.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const MyApp());
 }
 
